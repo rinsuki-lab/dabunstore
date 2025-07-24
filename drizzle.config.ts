dd039b116cbdac58cac1910762529aea40f46184
rinsuki <428rinsuki+git@gmail.com>
2025-07-24 16:16:11 +0900

    fi

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   out: './drizzle',
-  schema: './src/db/schema.ts',
+  schema: process.env.NODE_ENV === "production" ? './dist/db/schema.js' : './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
