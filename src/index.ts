4dcb506cc864b841e503f90c50132fe86f8f9da6
rinsuki <428rinsuki+git@gmail.com>
2025-07-24 14:56:35 +0900

    Viteが吐いたファイルをnodeでホスト

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { serve } from '@hono/node-server';
 import { Hono } from "hono";
 import routes from './routes/index.js';
+import { serveStatic } from '@hono/node-server/serve-static';
 
 const app = new Hono();
 
-app.get("/", async c => {
-    return c.text("Hello, World!");
-})
-
 app.route('/', routes);
+
+app.use("/assets/*", serveStatic({ root: "./dist/frontend/" }))
+app.use("*", serveStatic({ path: "./dist/frontend/index.html" }))
 
 serve(app)
